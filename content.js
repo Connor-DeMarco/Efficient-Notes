04eb266ce40d2331d82b86d5b13b87b55b5bb568
Christopher Rempe <43194904+MrRicearonie@users.noreply.github.com>
2021-06-06 22:02:49 -0700

    Data is retained from storage

--- content.js
+++ content.js
@@ -12,22 +12,32 @@ function highlightSelection(string) {
   wrapper.classList.add("notes-highlight");
 
   // Extract and save text content
-  var message = "\n* " + wrapper.textContent.trim() + "\n";
+  var message = "\n" + wrapper.textContent.trim() + "\n";
 
   // Send text content to sidebar
-  browser.runtime.sendMessage({message: message});
+  browser.runtime.sendMessage({ message: message });
 }
 
 // Copies all highlighted text to the clipboard in HTML format
 function copyAll() {
-  const spans = document.querySelectorAll(".notes-highlight");
+  const spans = browser.storage.local.get(window.location.toString()).then(onGot, onError);
+  // const spans = document.querySelectorAll(".notes-highlight");
+}
+
+// Copy the local data to the clipboard
+function onGot(item) {
+  const spans = item[window.location.toString()];
+  const lines = spans.split(/\r\n|\n\r|\n|\r/);
   var s = "<h2>" + window.location.toString() + "</h2>";
   s += "\n<ul>"
-  for (var i = 0; i < spans.length; i++) {
-    s += "\n\t";
-    s += "<li>";
-    s += spans[i].textContent.trim();
-    s += "</li>";
+  for (var i = 0; i < lines.length; i++) {
+    let line = lines[i].trim();
+    if (line.length != 0) {
+      s += "\n\t";
+      s += "<li>";
+      s += line;
+      s += "</li>";
+    }
   }
   s += "\n</ul>"
 
@@ -41,6 +51,10 @@ function copyAll() {
   document.addEventListener("copy", listener);
   document.execCommand("copy");
   document.removeEventListener("copy", listener);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
 }
 
 // Recieves messages from background, and calls the appropriate function
